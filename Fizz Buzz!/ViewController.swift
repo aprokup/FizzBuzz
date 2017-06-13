8839bd6cccfad2d0d978a775a5815a2c003d4d57
Alden Prokup <rprokup@aldens-macbook-air.local>
2017-06-13 09:57:59 -0500

    Initial Commit

--- Fizz Buzz!/ViewController.swift
+++ Fizz Buzz!/ViewController.swift
@@ -1,23 +1,59 @@
-//
 //  ViewController.swift
 //  Fizz Buzz!
-//
 //  Created by Alden Prokup on 6/7/17.
 //  Copyright © 2017 Alden Prokup. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myNumberLabel: UILabel!
+    var number = 0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myNumberLabel.text = "\(number)"
     }
+    @IBAction func screenTapped(_ sender: Any)
+    {
+        print("Screen Tapped")
+        number += 1
+        if number % 15 == 0
+        {
+        print ("Fizz Buzz")
+        view.backgroundColor = UIColor.red
+        myNumberLabel.text = "Fizz Buzz"
+        myNumberLabel.font = myNumberLabel.font.withSize(75)
+        }
+        else if number % 3 == 0
+        {
+        print("Fizz")
+        view.backgroundColor = UIColor.green
+        myNumberLabel.text = "Fizz"
+        }
+            else if number % 5 == 0
+            {
+            print("Buzz")
+            view.backgroundColor = UIColor.purple
+            myNumberLabel.text = "Buzz"
+            }
+        else
+        {
+            myNumberLabel.text = "\(number)"
+            view.backgroundColor = UIColor.black
+            myNumberLabel.font = myNumberLabel.font.withSize(100)
+        }
+        
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func longPress(_ sender: UILongPressGestureRecognizer)
+    {
+        print("Reset")
+        number = 0
+        myNumberLabel.text = "\(number)"
+        view.backgroundColor = UIColor.black
+        myNumberLabel.font = myNumberLabel.font.withSize(100)
     }
 
 
